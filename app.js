5c3bfb39806cef01beece3a3d4636f2d5f101cde
Brian Norman <briankn8@gmail.com>
2019-04-30 16:56:40 -0600

    Category Support (#13)
    * Implemented categories

--- app.js
+++ app.js
@@ -12,16 +12,23 @@ const annotateArticles = require("./lib/annotate.js")
 
 // Constants
 const LOCAL_PORT = 3000 // If this is being run locally then do it on this port
+const categoryList = ['all', 'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+
+function refreshAll() {
+	for (var i = 0; i < categoryList.length; i++){
+		refresh(categoryList[i])
+	}
+}
 
 // Prepares all the annotated articles for viewing
-function refresh(){
-    fetchAllArticles(store, status => {
+function refresh(category){
+    fetchAllArticles(store, category, status => {
         console.log("Fetching complete. " + (status.success ? "Success." : "Failure."))
     	if (status.success)
-		    prepArticles(store, status => {
+		    prepArticles(store, category, status => {
             console.log("Prepping complete. " + (status.success ? "Success." : "Failure."))
 		    	if (status.success)
-				    selectArticles(store, status => {
+				    selectArticles(store, category, status => {
                         console.log("Selecting complete. " + (status.success ? "Success." : "Failure."))
 
 				    	if (status.success)
@@ -31,6 +38,7 @@ function refresh(){
     })
 }
 
+
 // ~~~~~~~~~~~ Endpoint requests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // When a request is made to root
@@ -39,49 +47,58 @@ app.get('/', function (req, res) {
 	res.send('Welcome to the Factfulnews API!') 
 })
 
-// Request to /all
+
 // Returns a JSON array of articles on success, otherwise undefined on error
-app.get('/all', function (req, res, next) {
-    // This should just send the articles
-    store.load('all', function(err, articleObj){
-    	if (err) {
-    		next(err)
-        	console.log("Error when reading JSON during endpoint call in /all")
-       	}
+app.get('/:category', function (req, res, next) {
+	category = req.params.category
+	if (categoryList.includes(category)){
+		store.load(category, function(err, articleObj){
+			if (err) {
+				next(err)
+				console.log("Error when reading JSON")
+			}
 
-       	for (let i = 0; i < articleObj.articles.length; i++) {
-       		articleObj.articles[i].text = undefined
-       	}
+			for (let i = 0; i < articleObj.articles.length; i++) {
+				articleObj.articles[i].text = undefined
+			}
 
-        res.send(JSON.stringify({"articles": articleObj.articles}))
-    })
+			res.send(JSON.stringify({"articles": articleObj.articles}))
+		})
+	}
+	else {
+		res.send(category + " is not one of the approved categories.")
+	}
 })
 
-// Request to /all/article
+
 // Returns a string with HTML of the article body or URL to the article on error.
-app.get('/all/article', function (req, res, next) {
-	// Retrieve the specific article asked for in the link
-    // GET /all/article?id=<articleID>
+app.get('/:category/article', function (req, res, next) {
+	
     let articleID = req.query.id
+	category = req.params.category
+	if (categoryList.includes(category)){
+		store.load(category, function(err, articleObj) {
+			if (err) {
+				next(err)
+				console.log("Error when reading JSON")
+			}
 
-    store.load('all', function(err, articleObj) {
-        if (err) {
-        	next(err)
-        	console.log("Error when reading JSON during endpoint call in /all/article")
-       	}
-
-        res.send(JSON.stringify(articleObj.articles[articleID]))
-
-    })
+			res.send(JSON.stringify(articleObj.articles[articleID]))
+		})
+	}
+	else {
+		res.send(category + " is not one of the approved categories.")
+	}
 })
+
 
 // ~~~~~~~~~~~ Startup the backend and repeat every midnight ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Pipeline for getting the annotated articles
-refresh() 
+refreshAll() 
 cron.schedule('0 0 * * *', () => {
     console.log('Crom job refreshing articles at midnight');
-    refresh() 
+    refreshAll()  
 }, {
 	scheduled: true,
 	timezone: 'America/Regina'

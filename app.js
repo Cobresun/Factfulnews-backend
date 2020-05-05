3066fc12e6a1935e6cd8fe52eef3f1ca4ea208b7
Brian Norman <b2norman@uwaterloo.ca>
2020-05-05 09:44:27 -0600

    Support CORS

--- app.js
+++ app.js
@@ -33,8 +33,18 @@ async function refresh(category) {
 }
 
 
-// ~~~~~~~~~~~ Endpoint requests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~ CORS Support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+}
 
+app.use(allowCrossDomain)  
+
+
+// ~~~~~~~~~~~ Endpoint requests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // When a request is made to root
 app.get('/', function (req, res) {
 	// Return simple message
